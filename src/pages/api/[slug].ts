eb37206c0e8d2e34e8ba3a67fd275f5fbcd52aa9
Cold23 <envans.doni@gmail.com>
2022-06-18 22:30:53 +0300

    Added slug redirect

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../db/client";
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+
+    const slug = req.query["slug"];
+
+    if (!slug || typeof slug !== "string") {
+        res.status(404).json(JSON.stringify({ message: "Use a slug" }));
+        return
+    }
+
+    const data = await prisma.shortLink.findFirst({
+        where: {
+            slug: {
+                equals: slug
+            }
+        }
+    })
+
+    if (!data) {
+        res.status(404).json(JSON.stringify({ message: "Slug not found" }));
+        return
+    }
+
+    res.redirect(data.url!);
+}
